28f7685cbcbdc25d093fe9f04f04b25bb498f28a
jobinjose <jose.jobiin@gmail.com>
2025-04-12 23:13:22 +0530

    feat: error solved

--- netflix-frontend/src/utils/replacer.ts
+++ netflix-frontend/src/utils/replacer.ts
@@ -1,9 +1,6 @@
-import store from 'src/store';
 // import { IUserDetails } from 'src/types/user';
 
 export function replaceValues(data: string) {
-  const { app } = store.getState();
-  const { auth } = app;
   // const userDetails: IUserDetails = auth?.data;
 
   const replacements = [
